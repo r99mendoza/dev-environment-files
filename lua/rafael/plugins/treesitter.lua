288cbf2cef6a6b414df73d967d3d4156cd108fb2
Rafael Mendoza <rafaelmendoza7@gmail.com>
2024-08-31 02:07:47 -0500

    updated treesitter config

--- lua/rafael/plugins/treesitter.lua
+++ lua/rafael/plugins/treesitter.lua
@@ -2,22 +2,9 @@ return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
-  -- dependencies = {
-  --   "windwp/nvim-ts-autotag",
-  -- },
   config = function()
-    local treesitter = require("nvim-treesitter.configs")
-
-    treesitter.setup({
-      highlight = { -- enable syntax highlighting
-        enable = true,
-        disable = { "verilog" },
-      },
-      indent = { enable = true },
-      auto_install = true, -- Set false if no internet.
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      -- autotag = { enable = true },
-      -- ensure these language parsers are installed
+    require("nvim-treesitter.configs").setup({
+      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
       ensure_installed = {
         "bash",
         "cpp",
@@ -38,6 +25,31 @@ return {
         "vimdoc",
         "yaml",
       },
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally or have no internet access.
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
+        -- list of language that will be disabled
+        disable = { "verilog" },
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        -- disable = function(lang, buf)
+        --   local max_filesize = 100 * 1024 -- 100 KB
+        --   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        --   if ok and stats and stats.size > max_filesize then
+        --     return true
+        --   end
+        -- end,
+      },
+
+      -- Indentation based on treesitter for the = operator. NOTE: This is an experimental feature.
+      indent = { enable = true },
+
+      -- Incremental selection based on the named nodes from the grammar.
       incremental_selection = {
         enable = true,
         keymaps = {
